a844db058c34494d817b680b18d9c0be0c467d5c
Kevin <appzer@users.noreply.github.com>
2016-10-18 10:59:08 +0200

    Update pushsafer.sh

--- pushsafer.sh
+++ pushsafer.sh
@@ -10,5 +10,6 @@ SOUND="$5"
 VIBRATION="$6"
 URL="$7"
 URLTITLE="$8"
+TIME2LIVE="$9"
 
-curl https://www.pushsafer.com/api -d k="$PRIVATEKEY" -d m="$MESSAGE" -d t="$TITLE" -d d="$DEVICE" -d i="$ICON" -d s="$SOUND" -d v="$VIBRATION" -d u="$URL" -d ut="$URLTITLE"
+curl https://www.pushsafer.com/api -d k="$PRIVATEKEY" -d m="$MESSAGE" -d t="$TITLE" -d d="$DEVICE" -d i="$ICON" -d s="$SOUND" -d v="$VIBRATION" -d u="$URL" -d ut="$URLTITLE" -d l="$TIME2LIVE"
